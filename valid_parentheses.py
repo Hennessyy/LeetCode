2d22f22fd4f0ebf7649e640e0a548e9f3786ab01
Hennessyy <leon.hennessyy@gmail.com>
2022-01-05 21:16:33 +0000

    valid parentheses

---
+++ valid_parentheses.py
@@ -0,0 +1,30 @@
+def is_valid(s):
+
+    if len(s) % 2 != 0:
+        return False
+
+    dt = {
+        '{' : '}',
+        '[' : ']',
+        '(' : ')' 
+    } 
+
+    stk = []
+    
+    for i in range(len(s)):
+        if s[i] in dt:
+            stk.append(s[i])
+        elif len(stk) != 0 and dt[stk[-1]] == s[i]:
+            stk.pop()
+            continue
+        else:
+            return False
+    
+    if len(stk) != 0:
+        return False
+    else:
+        return True
+            
+        
+
+print(is_valid("(("))
