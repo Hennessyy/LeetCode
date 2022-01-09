057796d7adca84d0ebd61b19a2f627229c41dd31
Hennessyy <leon.hennessyy@gmail.com>
2022-01-09 23:00:44 +0000

    fixed

--- remove_duplicates.py
+++ remove_duplicates.py
@@ -2,9 +2,11 @@ def removeDuplicates(nums):
     left = 1
 
     for right in range(1, len(nums)):
-        if nums[right] != nums[left]:
+        if nums[right] != nums[right - 1]:
             nums[left] = nums[right]
             left += 1
         
-    return left
-        
+    return nums
+    
+
+print(removeDuplicates([0,0,1,1,1,2,2,3,3,4]))
